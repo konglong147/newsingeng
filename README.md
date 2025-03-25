07536f9fab1c662c86ec36e76b4f480c6a606a6a
konglong147 <freekonglong@163.com>
2025-03-24 19:57:57 -0700

    tss

--- README.md
+++
@@ -1,6 +0,0 @@
-# sing
-
-![test](https://github.com/konglong147/newsingeng/actions/workflows/test.yml/badge.svg)
-![lint](https://github.com/konglong147/newsingeng/actions/workflows/lint.yml/badge.svg)
-
-Do you hear the people sing?
--- common/json/unmarshal_context.go
+++
@@ -1,9 +0,0 @@
-//go:build go1.20 && !without_contextjson
-
-package json
-
-import (
-	json "github.com/konglong147/newsingeng/common/json/internal/contextjson"
-)
-
-var UnmarshalDisallowUnknownFields = json.UnmarshalDisallowUnknownFields
